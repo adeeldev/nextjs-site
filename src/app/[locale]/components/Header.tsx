e639ef1a87d2aea3ee4b919d3a3cfde2ab4f7c1d
hammad <hammad@claraworld.com>
2024-11-11 04:04:42 -0800

    update logo

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -2,13 +2,15 @@
 import { Link } from '@/src/navigation'
 import { useTranslations } from 'next-intl'
 import { FC } from 'react'
-import GithubIcon from '../../icons/github'
-import LogoIcon from '../../icons/logo'
 import LangSwitcher from './LangSwitcher'
 import ThemeSwitch from './ThemeSwitch'
+import Image from 'next/image' // Import the Next.js Image component
+import icupidLogo from '../../icons/icupid-logo.png' // Import the PNG logo
+
 interface Props {
   locale: string
 }
+
 export const Header: FC<Props> = ({ locale }) => {
   const t = useTranslations('')
   return (
@@ -16,15 +18,13 @@ export const Header: FC<Props> = ({ locale }) => {
       <Link lang={locale} href='/'>
         <div className='flex flex-row items-center'>
           <div className='mb-2 h-14 w-14'>
-            <LogoIcon />
+            <Image src={icupidLogo} alt='iCupid Logo' layout="responsive" width={56} height={56} /> {/* Use Next.js Image component */}
           </div>
           <strong className='mx-2 select-none'>iCupid</strong>
         </div>
       </Link>
       <div className='flex flex-row items-center gap-3'>
-       
         <LangSwitcher />
-
       </div>
     </div>
   )
